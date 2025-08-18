2a39019d5c75c61b458e756ccb8b3dbbc916037d
ruahhakodesh-create <ruahhakodesh@gmail.com>
2025-08-18 20:46:59 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,2 @@
 self.addEventListener("install", (e) => self.skipWaiting());
 self.addEventListener("activate", (e) => self.clients.claim());
-// Na razie bez cache; tylko po to, by PWA dawało „Zainstaluj”
